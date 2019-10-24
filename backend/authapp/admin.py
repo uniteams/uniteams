f7956e714166c3c97601ed42d2baca4d88b7661f
tum0xa <timofey.samodurov@mail.ru>
2019-10-24 23:49:58 +0500

    Added models UnieamsTeam, UniteamsGroup, OrgStruct

--- backend/authapp/admin.py
+++ backend/authapp/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
 
-from authapp.models import UniteamsUser, UserProfile
+from authapp.models import UniteamsUser, UserProfile, Company, UniteamsGroup, UniteamsTeam, OrgStruct
 
 admin.site.register(UniteamsUser)
 admin.site.register(UserProfile)
+admin.site.register(Company)
+admin.site.register(UniteamsTeam)
+admin.site.register(UniteamsGroup)
+admin.site.register(OrgStruct)
